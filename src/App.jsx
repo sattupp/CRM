d8350dbb8f761cc4a7a78b4d456b79fa9aec3d50
Satya <prakashsatya76453@gmail.com>
2024-06-12 12:56:17 +0530

    crud operations updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,28 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-import HomePage from "./pages/home/Home";
+import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./pages/cart/CartPage";
+import AllProduct from "./pages/allProduct/AllProduct";
+import Signup from "./pages/registration/Signup";
+import Login from "./pages/registration/Login";
 
 const App = () => {
   return (
     <div>
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
+          <Route path="/productinfo" element={<ProductInfo />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/allproduct" element={<AllProduct />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
